db1a43fac70630391f2c33cf43713317f5f95b9d
Sanjay Jain <sanjay21121973jain@gmail.com>
2018-06-06 15:07:52 +0530

    Add files via upload

---
+++ class4/prompt/myjs.js
@@ -0,0 +1,6 @@
+function sanjay(){
+firstValue=Number(document.getElementById("aaaa").value);
+secondValue=Number(document.getElementById("bbbb").value);
+c=firstValue+secondValue;
+document.getElementById("cccc").value=c;
+}
